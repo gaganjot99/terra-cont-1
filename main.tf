0dc9931353c313fe41709c3b94c172d2c22f6265
EC2 Default User <ec2-user@ip-172-31-81-140.ec2.internal>
2024-09-28 21:24:30 +0000

    added the ec2-code

--- main.tf
+++ main.tf
@@ -23,16 +23,15 @@ resource "aws_instance" "server" {
   ami           = "ami-0ebfd941bbafe70c6"
   instance_type = "t2.micro"
 
- 
+  security_groups = [aws_security_group.allow_ssh.id]
   subnet_id = data.aws_subnet.public.id
   
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
-              sudo amazon-linux-extras install docker -y
+              sudo yum install -y docker
               sudo service docker start
               sudo usermod -a -G docker ec2-user
-              sudo chkconfig docker on
               EOF
 
 
@@ -40,4 +39,35 @@ resource "aws_instance" "server" {
     Name = "server"
   }
   
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_sg"
+  description = "Security group to allow SSH access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
+    ingress {
+    from_port   = 8081
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_security_group"
+  }
 }
